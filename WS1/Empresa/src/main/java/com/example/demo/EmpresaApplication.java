0832705df8d1f096f1da23febf374a7fe1df7221
Yilool <20010917chen@gmail.com>
2020-10-19 13:41:39 +0200

    .

--- WS1/Empresa/src/main/java/com/example/demo/EmpresaApplication.java
+++ WS1/Empresa/src/main/java/com/example/demo/EmpresaApplication.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@EnableJpaRepositories
+@EntityScan(basePackages  = {"com.empresa.entity"})
 @SpringBootApplication(scanBasePackages = {"com.empresa"})
 public class EmpresaApplication{
 
